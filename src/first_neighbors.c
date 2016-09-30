21760ec0a619ef6d16a637162e21157b043b76e5
camilo <camilo@debian>
2016-09-30 15:56:22 -0500

    Funcionando hasta el conteo de cantidad de triangulos al cual pertenece. Me dispongo a hacer la función Masscenter(*Bunch), que encuentra el centro de masa de una distribución de puntos Particles.

--- src/first_neighbors.c
+++ src/first_neighbors.c
@@ -5,6 +5,7 @@
 
 struct Particle
 {
+  int id;
   int *FNid;
   float *Pos;
   float *FNdistance;
@@ -16,8 +17,8 @@ struct Particle
 /*Declaro las funciones*/
 struct Particle *Getdata(const char *inputfile, int NFN, int *num);
 float Distance(struct Particle *part1,struct Particle *part2);
-void Compare(struct Particle *part1,int part1id,struct Particle *part2,int part2id,int NFN);
-void Gettrinagles(struct Particle *part1,int part1id,struct Particle *part2,int part2id);
+void Compare(struct Particle *part1,struct Particle *part2,int NFN);
+void Gettriangles(struct Particle *bunch, int NFN,int num);
 
 
 int main(int argc,char *argv[] )
@@ -52,18 +53,19 @@ int main(int argc,char *argv[] )
     {
       for (int j=i+1; j<num;j++)
 	{
-	  printf("i= %i ,j= %i\n",i,j);
-	  Compare(&bunch[i],i,&bunch[j],j, NFN);
+	  //printf("i= %i ,j= %i\n",i,j);
+	  Compare(&bunch[i],&bunch[j], NFN);
 	}
     }
+  //Lleno el número de triangulos al que pretenece 
+  Gettriangles(bunch, NFN, num);
   for (int i=0; i<num; i++)
     {
       printf ("distancia[%i]: %f\n",i,bunch[i].FNdistance[0]);
       printf ("distancia[%i]: %f\n",i,bunch[i].FNdistance[1]);
+      printf ("Ntri[%i]: %i\n",i,bunch[i].Ntri);
+
     }
-  printf ("distancia[7]: %f\n",bunch[7].FNdistance[0]);
-  printf ("distancia[7]: %f\n",bunch[7].FNdistance[1]);
-  
   return 0;
   
 }//End main
@@ -115,7 +117,8 @@ struct Particle *Getdata(const char *inputfile, int NFN, int *num)
       //Mientras la linea no se acabe, lee elementos separados por ,- y espacios
       pch1 = strtok (line," ,"); //Para contar
       pch =pch1; //Para llenar
-
+      //Inicializa y marca la estructura con el indica.
+      bunch[i].id=i;
       //Inicializa el vector de indices de primeros vecinos
       bunch[i].FNid=malloc(sizeof(int)*NFN);
       //Inicializa el vector de distancias de primeros vecinos
@@ -161,12 +164,14 @@ float Distance(struct Particle *part1,struct Particle *part2)
 };
 
 
-void Compare(struct Particle *part1,int part1id,struct Particle *part2,int part2id,int NFN)
+void Compare(struct Particle *part1,struct Particle *part2,int NFN)
 {
   float dist;
   dist=Distance(part1, part2);
-  printf("Distancia=%f\n",dist);
+  //printf("Distancia=%f\n",dist);
   int condition=0;
+  int part1id=part1->id;
+  int part2id=part2->id;
   
   for(int i=0; i<NFN;i++)
     {
@@ -201,8 +206,8 @@ void Compare(struct Particle *part1,int part1id,struct Particle *part2,int part2
 		  part1->FNid[i]=part2id;
 		}
 	}
-      printf("part1->FNid[%i]= %i\n",i,part1->FNid[i]);
-      printf("part1->FNdistance[%i]=%f \n",i,part1->FNdistance[i]);
+      //printf("part1->FNid[%i]= %i\n",i,part1->FNid[i]);
+      //printf("part1->FNdistance[%i]=%f \n",i,part1->FNdistance[i]);
     }
   
   for(int i=0; i<NFN;i++)
@@ -242,4 +247,32 @@ void Compare(struct Particle *part1,int part1id,struct Particle *part2,int part2
       //printf("part2->FNdistance[%i]=%f \n",i,part2->FNdistance[i]);      
     }
   
+};
+
+void Gettriangles(struct Particle *bunch,int NFN, int num)//Tener cuidado con el doble conteo
+{
+  //printf("1");
+  
+  for(int i=0; i<num;i++)//Contador para la primera particula en el arreglo 
+    {
+      bunch[i].Ntri=0;
+      //printf("2");
+      for(int j=i; j<num;j++)//Contador para la segunda.
+	{
+	  //printf("2");
+	  for(int k=0; k<NFN;k++)//primer Contador de indice de vecinos
+	    {
+	      //printf("3");
+	      for(int l=0; l<NFN;l++)//Segundo Contador de indice de vecinos
+		{
+		  //printf("4");
+		  if(bunch[i].FNid[k]==bunch[j].FNid[l])
+		    {
+		      bunch[i].Ntri++;
+		      bunch[j].Ntri++;
+		    }
+		}
+	    }
+	}
+    }
 };
